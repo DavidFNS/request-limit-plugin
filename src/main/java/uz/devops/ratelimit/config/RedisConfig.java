61b46e28631c8d8b565299405ec3f8920a1383fe
Dovud <foundation240897@gmail.com>
2023-05-13 12:17:06 +0500

    deleted redis cache

--- src/main/java/uz/devops/ratelimit/config/RedisConfig.java
+++ src/main/java/uz/devops/ratelimit/config/RedisConfig.java
@@ -1,22 +1,15 @@
 package uz.devops.ratelimit.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-@Configuration
-public class RedisConfig {
-    @Bean
-    @Primary
-    StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate redisTemplate = new StringRedisTemplate();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setEnableTransactionSupport(false);
-        redisTemplate.afterPropertiesSet();
-
-        return redisTemplate;
-    }
-}
+//@Configuration
+//public class RedisConfig {
+//    @Bean
+//    @Primary
+//    StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//        redisTemplate.setEnableTransactionSupport(false);
+//        redisTemplate.afterPropertiesSet();
+//
+//        return redisTemplate;
+//    }
+//}
--- src/main/java/uz/devops/ratelimit/repository/LimitBucketRedisRepository.java
+++ src/main/java/uz/devops/ratelimit/repository/LimitBucketRedisRepository.java
@@ -1,8 +1,5 @@
 package uz.devops.ratelimit.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import uz.devops.ratelimit.domain.LimitBucketRedis;
-
 //@Repository
-public interface LimitBucketRedisRepository extends CrudRepository<LimitBucketRedis, Long> {
-}
+//public interface LimitBucketRedisRepository extends CrudRepository<LimitBucketRedis, Long> {
+//}
